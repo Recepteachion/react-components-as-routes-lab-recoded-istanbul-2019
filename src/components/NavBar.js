2d5d90abbfc8a492b9f494117e0b07cd53729854
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-08 17:28:02 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,15 @@ const NavBar = () => {
       <NavLink to="/"
                exact
                >Home</NavLink>
+      <NavLink to="/movies"
+               exact
+               >Movies</NavLink>
+      <NavLink to="/directors"
+               exact
+               >Directors</NavLink>
+      <NavLink to="/actors"
+               exact
+               >Actors</NavLink>
     </div>
   );
 };
