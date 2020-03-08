e143bc4858a1d1d67c4a410b80b6b5ec273767f5
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-08 18:14:10 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,19 +6,15 @@ const Movies = () => {
     <div>
         {/*{code here}*/}
         <h1>Movies Page</h1>
-        {movies.map(movie =>
-           (
+        {movies.map(movie => (
           <div>
           <h2>{movie.title}</h2>
           <p>{movie.time}</p>
           <ul>{movie.genres.map(genre => <li>{genre}</li>)}</ul>
           </div>
-        )
-        )
-        
-        
-    </div>
-  );
+        ))}
+      </div>
+  )
 };
 
 export default Movies;
